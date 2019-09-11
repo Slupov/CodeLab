5544618f115fce5ae17a032401b1ea926a29cf2d
Slupov <stoyan.lupov@gmail.com>
2019-09-11 15:46:07 +0300

    [Removed build files from repo]

--- C++/build/asapcppadvanced/CMakeFiles/asapcppadvanced.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libasapcppadvanced.pdb"
-  "libasapcppadvanced.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/asapcppadvanced.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
